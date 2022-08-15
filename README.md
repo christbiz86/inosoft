7859dada471d6800608da4951330cf45e08b4861
Christian Kurniawan <christian.k@smartfren.com>
2022-08-15 09:20:32 +0700

    first init

---
+++ README.md
@@ -0,0 +1,16 @@
+## Tutorial Instalasi Inosoft Test Laravel
+
+
+[comment]: <> (Laravel is a web application framework with expressive, elegant syntax. We believe development must be an enjoyable and creative experience to be truly fulfilling. Laravel takes the pain out of development by easing common tasks used in many web projects, such as:)
+
+- Download library yang diperlukan dengan command : composer update
+- Migrasi database dengan command : php artisan migrate
+- Start service laravel dengan command : php artisan serve
+
+Create User
+- Menggunakan API :
+- 
+
+
+Laravel is accessible, powerful, and provides tools required for large, robust applications.
+
---
+++ app/Http/Middleware/Authenticate.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Tymon\JWTAuth\JWTAuth;
+
+class Authenticate extends Middleware
+{
+    /**
+     * Get the path the user should be redirected to when they are not authenticated.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return string|null
+     */
+    protected function redirectTo($request)
+    {
+//        if (! $request->expectsJson()) {
+//            return route('login');
+//        }
+        if(empty($request->header('Authorization'))){
+            header('HTTP/1.0 401');
+            echo 'Unauthorization Access!!!';
+            die();
+        } else {
+            $user = JWTAuth::parseToken()->authenticate();
+            var_dump($user);
+        }
+    }
+}
